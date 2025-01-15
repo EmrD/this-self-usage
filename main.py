684d52c4edec2d006e080e8de60cc44f1f3f2d98
Emr <127445447+EmrD@users.noreply.github.com>
2025-01-15 18:49:05 +0300

    Python file added.

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ project1 = Project(name = "project1") # Creating the objects
 
 project1.compile() # projectName is now project1
 project2.name = "test_for_project_2" # We are overriding the name variable. The default value must be project2
+
 Project.compile(project2) # projectName is now test_for_project_2
